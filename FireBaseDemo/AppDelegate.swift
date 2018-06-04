342d61c0cc767e9a7567e478b936f9fd43db68a2
M.REHAN <asim.r.k@outlook.com>
2018-06-04 05:37:11 -0700

    First Commit

--- FireBaseDemo/AppDelegate.swift
+++ FireBaseDemo/AppDelegate.swift
@@ -8,42 +8,107 @@
 
 import UIKit
 import CoreData
+import Firebase
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,MessagingDelegate {
 
     var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        
+        
+        
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        
+        
+        
+        
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        
+         UIApplication.sharedApplication().registerForRemoteNotifications()
+        
+        
+        
         return true
     }
-
+    func messaging(messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        
+        //let token = Messaging.messaging().fcmToken
+        print("fcm token:\(fcmToken)")
+        
+        
+        
+        
+    }
+    
+    func messaging(messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        
+        
+    }
+    
+    func messaging(messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+        
+        
+        print(remoteMessage.appData)
+        
+        
+        
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+       
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+       
+        
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+       
+        
     }
 
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
+      
         self.saveContext()
     }
 
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        
+       Messaging.messaging().apnsToken = deviceToken
+       
+        
+        Messaging.messaging().apnsToken = deviceToken
+        Messaging.messaging().delegate = self
+        let fcm_token = Messaging.messaging().fcmToken
+        print("FCM token: \(fcm_token ?? "")")
+        print("dwvicetoken\(deviceToken)")
+
+
+    }
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        
+    }
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print(error.localizedDescription)
+    }
+    
     // MARK: - Core Data stack
 
     lazy var applicationDocumentsDirectory: NSURL = {
---
+++ Pods/Target Support Files/Pods-FireBaseDemo/Pods-FireBaseDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireBaseDemo : NSObject
+@end
+@implementation PodsDummy_Pods_FireBaseDemo
+@end
